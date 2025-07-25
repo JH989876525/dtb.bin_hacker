05a141ec86aea6ac8b48e13454c30e989767aa2b
jh989876525 <james_chen@innodisk.com>
2025-07-25 17:00:11 +0800

    init.

---
+++ modify_qcom_dtb.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+# Copyright (c) 2025 innodisk Crop.
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+FILENAME=${1}
+
+if [ "${FILENAME##*.}" = "vfat" ] || [ "${FILENAME##*.}" = "bin" ]; then
+    echo "${FILENAME} is .vfat file."
+else
+    echo "${FILENAME} is not .vfat file."
+    exit
+fi
+
+mkdir -p vfat
+
+sleep 0.3
+
+MOUNT_POINT=$(realpath vfat)
+
+sudo mount -o loop -t vfat "${FILENAME}" "${MOUNT_POINT}"
+
+cd "${MOUNT_POINT}"
+
+sudo dtc -i dtb -o combined-dtb.dts combined-dtb.dtb
+
+code combined-dtb.dts
