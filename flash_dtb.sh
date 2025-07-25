fe9aa7c731a7efe3e63757ef0ed5fccffe0a487d
jh989876525 <james_chen@innodisk.com>
2025-07-25 17:47:23 +0800

    fix: fix " issue

--- flash_dtb.sh
+++ flash_dtb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 image_folder="$( cd "$( dirname "$0" )" && pwd )"
 
-qdl --storage ufs --include "$image_folder/" "$image_folder/prog_firehose_ddr.elf" "$image_folder/rawprogram*.xml"
+qdl --storage ufs --include $image_folder/ $image_folder/prog_firehose_ddr.elf $image_folder/rawprogram*.xml
